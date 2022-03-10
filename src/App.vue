bfad184c6099107842e8c6e2433ef1af7e1c2bfb
arifitantotito <arifitantotito@gmail.com>
2022-03-10 15:26:48 +0700

    todo-exercise

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <HelloWorld msg="What You Want To Do?"/>
+</template>
+
+<script>
+import HelloWorld from './components/to-do-list.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  width: 100%;
+  height: 100vh;
+  padding: 10px;
+  background: linear-gradient(#3dcdf1 0%, #5ee6eb 25%, #49e8b3 50%, #49f06d 100%);
+}
+</style>
---
+++ src/components/to-do-list.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="box">
+    <h1 class="text-center">What Do You Want To Do?</h1>
+    <input        
+        class="input fw-bold"
+        id="inputTodo"
+        placeholder="Write Down"
+        autocomplete="off"
+        type="text"
+        v-model="newTodo"
+        @keyup.enter="addTodo" />
+
+    <button class="btn btn-dark" @click="addTodo">+</button>
+    
+    <div class="box2 mt-4">
+      <div class="boxx card my-2" v-for="(todo, index) in todos" :key="index">
+        <div class=" card-body row">
+          <div class="col-10 my-auto text-dark fw-bold text-align-start">
+            {{ todo.text }}
+          </div>
+            <div class="col-2 mt-auto">
+              <button @click="removeTodo(todo)" class="btn2 btn-dark"> DONE </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+let id  = 0
+export default {
+ data() {
+    return {
+      newTodo: '',
+      todos: [
+      
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo })
+      this.newTodo = ''
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo)
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+
+  h1 {
+    font-family: 'Lobster', cursive;
+  }
+  .box {
+    background: transparent;
+    max-width: 400px;
+    width: 100%;
+    margin: 120px auto;
+    padding: 25px;
+    border-style: inset ;
+    border-color: black;
+    border-width: 4.5px;
+    border-radius: 20px;
+    box-shadow: 0px 10px 15px rgba(22, 16, 16, 0.1);
+  }
+
+  .input {
+    margin-left: 11%;
+    padding: 2%;
+    margin-top: 3%;
+    border-radius: 20px;
+    border-color: black;
+    background: transparent;
+    font-family: monospace;
+  }
+
+  .btn {
+    margin-left: 73.5%;
+    margin-top: -19.5%;
+    margin-bottom: 1%;
+    background: transparent;
+    border-radius: 100%;
+    padding-top: 1%;
+    padding-bottom: 1px;
+    font-size: 22px;
+  }
+  .btn:hover {
+    background-color: rgb(187, 142, 20);
+  }
+
+  .box2 {
+    margin-left: 11%;
+    padding: 1%;
+    margin-top: 1%;
+    border-radius: 20px;
+    border-color: black;
+    background: transparent;
+  }
+
+  .boxx {
+    background-color: rgb(255, 231, 231);
+    border-radius: 40px;
+    margin-left: -15%;
+    margin-right: -1%;
+    padding: 1%;
+    margin-top: 1%;
+    border-radius: 40px;
+    border-color: black;
+    background: rgb(190, 207, 192);
+    font-family: monospace;
+    
+  }
+  .btn2 {
+    background: transparent;
+    border-radius: 40%;
+    font-size: 79%;
+    color: transparent;
+    border-width: 2px;
+  }
+  .boxx:hover .btn2 {
+    display: block;
+     margin-left: -15%;
+    margin-right: -1%;
+    padding: 1%;
+    margin-top: 1%;
+    border-radius: 40px;
+  }
+  .boxx .btn2 {
+    display: none;
+  }
+  .btn2:hover {
+    background-color: rgb(220, 50, 20);
+    display: none;
+  }
+</style>
